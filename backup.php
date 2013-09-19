94a43db032e268aa583684835bef294a427c4de6
Oscar Dias <oscar.dias@softerize.com>
2013-09-19 18:26:31 -0300

    Adding missing / to the example

--- backup.php
+++ backup.php
@@ -27,7 +27,7 @@ $u1obj = new u1Backup();
 $u1obj->setDatabase('localhost', 'root', '', 'dbname');
 // or $u1obj->setDatabase('localhost', 'root', '', array('dbname1','dbname2'));
 
-$u1obj->setPaths('/tmp/', '~/Ubuntu One/backups');
+$u1obj->setPaths('/tmp/', '/~/Ubuntu One/backups');
 $u1obj->setUbuntuOne('email', 'pass');
 
 // Execute
